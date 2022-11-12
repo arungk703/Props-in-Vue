044a11aa9c1f322d32fbb557d4f789a5c523ccb8
arungk703 <100412061+arungk703@users.noreply.github.com>
2022-11-12 14:29:55 +0530

    Parent and Child components

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Child :name="name" :skills="skills"> </Child>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+export default {
+  name: "App",
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      name: "Arun",
+      skills: ["Java", "Python", "HTML", "CSS", "JS", "Vue.js", "Git"],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Child.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Hii, I am {{ name }}</h1>
+  <h2>My Skills are:</h2>
+  <h3
+    :class="changedClass"
+    @mouseover="isChanged"
+    v-for="(skill, index) in skills"
+    :key="index"
+  >
+    {{ skill }}
+  </h3>
+</template>
+
+
+<script>
+export default {
+  name: "child-component",
+  props: ["name", "skills"],
+  data() {
+    return {
+      changedClass: "normal",
+    };
+  },
+  methods: {
+    isChanged() {
+      this.changedClass = "promoted";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.promoted {
+  background-color: aquamarine;
+}
+.normal {
+  color: blue;
+}
+</style>
